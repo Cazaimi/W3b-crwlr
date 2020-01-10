76ec87bbaa94ed41fad77d7f68cce611589abb7a
Cazaimi <anmol01shukla@gmail.com>
2020-01-10 09:56:57 +0530

    Added URL formatting and same host functionality.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const orchestrator = require('./lib/orchestrator');
 
 let webpage = process.argv[2];
 
-orchestrator(webpage, (...results) => {
+orchestrator(webpage, { sameHost: true }, (...results) => {
   console.log('results:', results);
 });
