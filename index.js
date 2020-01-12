bc30ffd955577bcfb31159dfed71b6c698ca1b33
Cazaimi <anmol01shukla@gmail.com>
2020-01-12 20:22:36 +0530

    The crawler now operates when event are fired as opposed to a timer that runs periodically after a constant amount of time.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const orchestrator = require('./lib/orchestrator');
+const fs = require('fs'),
 
-let webpage = process.argv[2];
+  orchestrator = require('./lib/orchestrator');
 
-orchestrator(webpage, { sameHost: true }, (...results) => {
-  console.log('results:', results);
+let webpage = process.argv[2],
+  file = process.argv[3];
+
+orchestrator(webpage, { sameHost: true }, (err, results) => {
+  if (err) {
+    throw new Error({ error: err, message: 'The web crawler errored out' });
+  }
+
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  file && fs.writeFile(file, JSON.stringify(results), process.exit);
+
+  // If the user has not given a file, then print the web tree to the console.
+  if (!file) {
+    console.info(results);
+  }
 });
